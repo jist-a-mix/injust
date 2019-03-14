d413177ed6131f446416bf04467db7e4b4167d2b
mik <autoclik974@gmail.com>
2019-03-14 06:34:54 +0100

    ok

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -24,4 +24,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor/scss')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
- Rails.application.config.assets.precompile += %w( admin.js admin.css )
+ 
