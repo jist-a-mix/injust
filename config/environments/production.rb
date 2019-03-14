b3f922ae2c6f731714ec42f844cc3520a3fcd0b9
mik <autoclik974@gmail.com>
2019-03-14 21:50:13 +0100

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  recaptcha_public_key= ENV['PUBLIC KEY']
+  recaptcha_private_key= ENV['PRIVATE KEY']
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
