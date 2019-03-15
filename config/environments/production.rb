3e37d661aab3f52b9ce23cf6969e54f8b056e4dc
mik <autoclik974@gmail.com>
2019-03-15 22:19:17 +0100

    view fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
    
   # SMTP settings for gmail
-  config.action_mailer.default_url_options = { :host => "injustice-world.herokuapp.com"}
+  config.action_mailer.default_url_options = { :host => "https://injustice-world.herokuapp.com"}
   
   config.action_mailer.delivery_method = :smtp
    config.action_mailer.perform_deliveries = true
